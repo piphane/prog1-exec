66aeb79b16386905e96025a3de0be8faaaeb20b8
Mateus Koppe <mateus.koppe@gmail.com>
2019-09-16 21:59:26 -0300

    Cria classe data

---
+++ Data.java
@@ -0,0 +1,29 @@
+class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public int getDia () {
+        return this.dia;
+    }
+
+    public void setDia (int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes () {
+        return this.mes;
+    }
+
+    public void setMes (int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno () {
+        return this.ano;
+    }
+
+    public void setAno (int ano) {
+        this.ano = ano;
+    }   
+}
